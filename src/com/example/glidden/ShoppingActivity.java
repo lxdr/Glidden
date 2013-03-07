41b30973813b3f9f088a6c6c562d3ca5a459b92e
Alex Drawbond <alex.drawbond@drake.edu>
2013-03-07 08:26:26 -0600

    add some comments

--- src/com/example/glidden/ShoppingActivity.java
+++ src/com/example/glidden/ShoppingActivity.java
@@ -8,6 +8,14 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+/**
+ * This is the ShoppingActivity class. It will contain a list of
+ * projects to be completed with links to shopping lists for their
+ * supplies. 
+ * 
+ * @author alexdrawbond
+ *
+ */
 public class ShoppingActivity extends Activity {
 	Button button1;
 	ImageButton button2;
@@ -21,7 +29,7 @@ public class ShoppingActivity extends Activity {
 		button1 = (Button) findViewById(R.id.button1); 
 		button2 = (ImageButton) findViewById(R.id.image_button);
 		
-		//go to Camera class when Camera is clicked
+		//go to MainActivity class when Camera is clicked
         button1.setOnClickListener(new OnClickListener()
         {
         	@Override
@@ -34,7 +42,7 @@ public class ShoppingActivity extends Activity {
         	}
         });
         
-      //go to Camera class when Camera is clicked
+      //go to the supplies list when project is clicked
         button2.setOnClickListener(new OnClickListener()
         {
         	@Override
