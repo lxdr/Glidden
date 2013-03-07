5b3a9ca9d6b29969cf1f1874782d6f8f527e9562
Troy Whitmer <troy.whitmer@drka.edu>
2013-03-06 18:03:35 -0600

    troys additions

---
+++ src/com/example/glidden/Camera.java
@@ -0,0 +1,56 @@
+package com.example.glidden;
+
+import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+
+
+public class Camera extends Activity {
+	
+	Button button;
+	Button button1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+	
+		button = (Button) findViewById(R.id.button1);
+		button1 = (Button) findViewById(R.id.button2);
+        
+		button.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
+        		Intent intent = new Intent(v.getContext(), Share.class);
+        		startActivity(intent);
+        		
+        	}
+        });
+		
+		button1.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
+        		Intent intent = new Intent(v.getContext(), MainActivity.class);
+        		startActivity(intent);
+        		
+        	}
+        });
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_camera, menu);
+		return true;
+	}
+
+}
--- src/com/example/glidden/MainActivity.java
+++ src/com/example/glidden/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.glidden;
 
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
