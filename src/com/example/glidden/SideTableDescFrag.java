f609c1f586a11395121714f0332e549346aad458
Troy Whitmer <troy.whitmer@drka.edu>
2013-04-16 10:31:17 -0500

    pulling code

--- src/com/example/glidden/SideTableDescFrag.java
+++ src/com/example/glidden/SideTableDescFrag.java
@@ -1,6 +1,7 @@
 package com.example.glidden;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
--- src/com/example/glidden/WoodenSideProject.java
+++ src/com/example/glidden/WoodenSideProject.java
@@ -61,9 +61,11 @@ public class WoodenSideProject extends FragmentActivity {
 	            Intent intent = new Intent(this, Traditional.class);
 	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 	            startActivity(intent);
+	    		
 	            return true;
 	        default:
 	            return super.onOptionsItemSelected(item);
+	         
 	    }
 	}
 	
@@ -72,6 +74,7 @@ public class WoodenSideProject extends FragmentActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.action_menu, menu);
+		
 		return true;
 	}
 }
